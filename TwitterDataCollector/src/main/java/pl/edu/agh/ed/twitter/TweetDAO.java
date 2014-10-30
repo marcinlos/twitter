40195afebb87a5edac91a4a047da5bcd410280e2
los <losiu99@gazeta.pl>
2014-10-30 23:02:01 +0100

    Remove obsolete TweetDAO.getTweets()

--- TwitterDataCollector/src/main/java/pl/edu/agh/ed/twitter/TweetDAO.java
+++ TwitterDataCollector/src/main/java/pl/edu/agh/ed/twitter/TweetDAO.java
@@ -1,17 +1,11 @@
 package pl.edu.agh.ed.twitter;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
 import pl.edu.agh.ed.twitter.domain.Tweet;
 
 @Repository
 public class TweetDAO extends BaseDAO<Long, Tweet> {
-    
-    public List<Tweet> getTweets() {
-        return (List<Tweet>) session().createQuery("from Tweet").list();
-    }
     
     @Override
     protected Class<Tweet> getEntityClass() {
