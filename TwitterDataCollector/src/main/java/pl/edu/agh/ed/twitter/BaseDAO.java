e70f452342ff7489583e96e4cfe9613865b7a289
los <losiu99@gazeta.pl>
2014-10-30 22:59:31 +0100

    Add counting and listing methods to BaseDAO

--- TwitterDataCollector/src/main/java/pl/edu/agh/ed/twitter/BaseDAO.java
+++ TwitterDataCollector/src/main/java/pl/edu/agh/ed/twitter/BaseDAO.java
@@ -1,9 +1,16 @@
 package pl.edu.agh.ed.twitter;
 
 import java.io.Serializable;
+import java.util.List;
 
+import org.hibernate.ScrollMode;
+import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.StatelessSession;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -20,11 +27,40 @@ public abstract class BaseDAO<Id extends Serializable, Entity> {
     public SessionFactory sessionFactory() {
         return sessionFactory;
     }
+    
+    public StatelessSession openStatelessSesion() {
+        return sessionFactory.openStatelessSession();
+    }
 
     public Session session() {
         return sessionFactory.getCurrentSession();
     }
     
+    public long count(Criterion... predicates) {
+        Number n = (Number) session()
+                .createCriteria(getEntityClass())
+                .add(Restrictions.and(predicates))
+                .setProjection(Projections.rowCount())
+                .uniqueResult();
+        return n.longValue();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Entity> getList(Criterion... predicates) {
+        return (List<Entity>) session()
+                .createCriteria(getEntityClass())
+                .add(Restrictions.and(predicates))
+                .list();
+    }
+    
+    public ScrollableResults getCursor(ScrollMode mode, Criterion... predicates) {
+        return session()
+                .createCriteria(getEntityClass())
+                .add(Restrictions.and(predicates))
+                .scroll(mode);
+    }
+    
+    @SuppressWarnings("unchecked")
     public Entity get(Id id) {
         return (Entity) session().get(getEntityClass(), id);
     }
