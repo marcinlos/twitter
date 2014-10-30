c0a6e3856fb39481b74df307a7c69266200a2f8c
los <losiu99@gazeta.pl>
2014-10-30 20:21:36 +0100

    Remove printTweet from TweetListener

--- TwitterDataCollector/src/main/java/pl/edu/agh/ed/twitter/TweetListener.java
+++ TwitterDataCollector/src/main/java/pl/edu/agh/ed/twitter/TweetListener.java
@@ -6,7 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import pl.edu.agh.ed.twitter.domain.Tweet;
 import pl.edu.agh.ed.twitter.domain.User;
-import twitter4j.Place;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
@@ -20,52 +19,6 @@ public class TweetListener implements StatusListener {
     
     @Autowired
     private UserDAO users;
-    
-    private void printTweet(Status tweet) {
-        System.out.println("- - - - - - - - - - - - - - - - - - -  - -");
-        twitter4j.User user = tweet.getUser();
-        System.out.println("Id: " + tweet.getId());
-        System.out.println("At: " + tweet.getCreatedAt());
-        System.out.println("Source: " + tweet.getSource());
-        System.out.println("User: " + user.getScreenName() + " [id=" + user.getId() + "]");
-        System.out.println("Text: \n<<<" + tweet.getText() + "\n>>>");
-
-        if (! tweet.getLang().equals("und")) {
-            System.out.println("Language: " + tweet.getLang());
-        }
-        if (tweet.getScopes() != null) {
-            System.out.println("Scopes: " + tweet.getScopes());
-        }
-        if (tweet.isRetweet()) {
-            Status retweeted = tweet.getRetweetedStatus();
-            System.out.println("Is retweet of: " + retweeted.getId());
-            System.out.println("               retweets: " + retweeted.getRetweetCount());
-            System.out.println("               favorites: " + retweeted.getFavoriteCount());
-        }
-        System.out.println("Is retweeted: " + tweet.isRetweeted());
-        System.out.println("Retweet count: " + tweet.getRetweetCount());
-        System.out.println("Is favorited: " + tweet.isFavorited());
-        System.out.println("Fav count: " + tweet.getFavoriteCount());
-        
-        System.out.println("In reply to status: " + tweet.getInReplyToStatusId());
-        System.out.println("In reply to user: " + tweet.getInReplyToUserId());
-        System.out.println("In reply to screen name: " + tweet.getInReplyToScreenName());
-        
-        Place place = tweet.getPlace();
-        if (place != null) {
-            System.out.println("Place:");
-            System.out.println(" - name: " + place.getName());
-            System.out.println(" - streetAddress: " + place.getStreetAddress());
-            System.out.println(" - id: " + place.getId());
-            System.out.println(" - country: " + place.getCountry());
-            System.out.println(" - place type: " + place.getPlaceType());
-            System.out.println(" - url: " + place.getURL());
-            System.out.println(" - full name: " + place.getFullName());
-        } else {
-            System.out.println("[No place]");
-        }
-        System.out.println("- - - - - - - - - - - - - - - - - - -  - -");
-    }
     
     @Override
     public void onException(Exception e) {
