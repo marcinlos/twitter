47a1fc0e28447268c876cb78cae84b3abdfe3ab9
los <losiu99@gazeta.pl>
2014-10-31 11:02:05 +0100

    Add Main for recommended users fetching

---
+++ TwitterDataCollector/src/main/java/pl/edu/agh/ed/twitter/GraphBuilder.java
@@ -0,0 +1,29 @@
+package pl.edu.agh.ed.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+@Configuration
+@ComponentScan(basePackages = "pl.edu.agh.ed.twitter")
+@ImportResource({
+    "classpath:spring.xml",
+})
+@EnableScheduling
+public class GraphBuilder {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(GraphBuilder.class, args);
+        Runnable fetcher = ctx.getBean(FetchRecommended.class);
+        
+        fetcher.run();
+//        Twitter twitter = TwitterFactory.getSingleton();
+        
+    }
+}
